b986312d373ee312b9752edb95c377413bbf4290
Patrick <patrick.a.mclennan@gmail.com>
2019-04-11 23:37:24 -0400

    styling, most routing done

--- client/src/components/Calendar/Calendar.style.ts
+++ client/src/components/Calendar/Calendar.style.ts
@@ -13,11 +13,14 @@ export const StyledGrid = styled.section`
   position: relative;
   display: grid;
   margin: 2.5vw;
-  grid-template-rows: 17.5vh repeat(auto-fit, minmax(250px, 1fr));
+  padding: 1rem;
+  grid-template-rows: 17.5vh repeat(auto-fill, minmax(100px, 1fr));
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   grid-auto-flow: row;
   grid-area: 1 / 1 / -1 / -1;
   grid-gap: 1rem;
+  align-items: center;
+  justify-content: center;
   box-shadow: inset 0 4px 8px 0 rgba(0, 0, 0, 0.12),
     inset 0 2px 4px 0 rgba(0, 0, 0, 0.08);
   background-image: radial-gradient(
--- client/src/components/Event/Event.style.ts
+++ client/src/components/Event/Event.style.ts
@@ -3,4 +3,5 @@ import styled from 'styled-components';
 export const StyledDiv = styled.div`
   border: 1px solid black;
   background-color: white;
+  align-self: stretch;
 `;
