11d6e856d62f6b4d55dfc8797579db383a0271c1
Patrick <patrick.a.mclennan@gmail.com>
2019-04-13 00:36:50 -0400

    styling, working on animations

--- client/src/components/Event/Event.style.ts
+++ client/src/components/Event/Event.style.ts
@@ -1,7 +1,17 @@
 import styled from 'styled-components';
 
 export const StyledDiv = styled.div`
-  ${({ theme: { elevation } }: any) => elevation.mainInset}
+  ${({ theme: { flexin } }: any) => flexin('space-evenly', 'center', 'column')}
+  ${({ theme: { elevation } }: any) =>
+    elevation.mainInset}
   background-color: rgba(255,255,255,.8);
   align-self: stretch;
+  transition: all 0.3s;
+  transform: scale(1);
+
+  &:hover,
+  &:active,
+  &:focus {
+    transform: scale(1.15);
+  }
 `;
