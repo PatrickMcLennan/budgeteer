c68f72ce461f7eaa1a9eb5818fb2241e752dafcb
Patrick <patrick.a.mclennan@gmail.com>
2019-04-13 22:41:29 -0400

    mostly done

--- client/src/components/FormModal/FormModal.style.ts
+++ client/src/components/FormModal/FormModal.style.ts
@@ -5,10 +5,12 @@ interface StyledFormProps {
 }
 
 export const StyledH2 = styled.h2`
-  font-size: 4rem;
+  grid-area: 1 / 1 / 2 / -1;
+  text-align: center;
+  font-size: 7.5rem;
   font-style: italic;
   color: rgba(0, 0, 0, 0.35);
-  line-height: 5;
+  line-height: 3.5;
 `;
 
 export const Backdrop = styled.div`
@@ -26,9 +28,10 @@ export const StyledForm = styled.form`
   ${({ theme: { elevation } }: any) => elevation.mainInset}
 
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px 350px));
+  grid-template-rows: 100px minmax(30px, 50px) minmax(30px, 50px) 200px;
+  grid-template-columns: repeat(minmax(175px, 250px));
 
-  padding: 5vh 5vw;
+  padding: 2.5vh 5vw;
   position: relative;
   background-color: rgba(255, 255, 255, 0.9);
   margin: 10vh 10vw;
@@ -41,19 +44,67 @@ export const StyledForm = styled.form`
   transition-delay: 0.1s;
   ${(props: StyledFormProps) =>
     props.visible && `transform: translateY(0); opacity: 1;`};
+
+  & label[for='name'] {
+    grid-area: 2 / 1 / 3 / 2;
+  }
+
+  & label[for='location'] {
+    grid-area: 3 / 1 / 4 / 4;
+  }
+
+  & label[for='description'] {
+    grid-area: 2 / 2 / 3 / 3;
+  }
+
+  & label[for='year'] {
+    grid-area: 3 / 2 / 4 / 3;
+  }
+
+  & label[for='month'] {
+    grid-area: 4 / 2 / 5 / 3;
+  }
+
+  & label[for='day'] {
+    grid-area: 5 / 3 / 3 / 4;
+  }
+
+  & label[for='date'] {
+    grid-area: 2 / 3 / 3 / -1;
+  }
+
+  & label[for='startTime'] {
+    grid-area: 2 / 3 / 3 / -1;
+  }
+
+  & label[for='endTime'] {
+    grid-area: 2 / 3 / 3 / -1;
+  }
+
+  & label[for='cost'] {
+    grid-area: 2 / 3 / 3 / -1;
+  }
+
+  & label[for='submit'] {
+    grid-area: -1 / 1 / -1 / -1;
+  }
 `;
 
 export const StyledInput = styled.input`
-  /* grid-area: 1 / 1 / -1 / -1; */
+  margin: 1rem auto;
+  border: none;
+  background: transparent;
+  border-bottom: 1px solid red;
   z-index: 7;
 `;
 
 export const StyledLabel = styled.label`
-  ${({ theme: { flexin } }: any) => flexin('center', 'flex-start', 'column')}
-  margin: 1rem;
+  ${({ theme: { flexin } }: any) =>
+    flexin('space-evenly', 'flex-start', 'column')}
   width: 60%;
   font-size: 1.5rem;
   text-transform: uppercase;
   font-style: italic;
   color: rgba(0, 0, 0, 0.7);
+  border: 1px solid black;
 `;
