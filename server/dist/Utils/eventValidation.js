b472a3add134603ac30521ff1104f0380885a8b1
Patrick <patrick.a.mclennan@gmail.com>
2019-04-16 17:26:56 -0400

    event sorting is working on the back-end

--- server/dist/Utils/eventValidation.js
+++ server/dist/Utils/eventValidation.js
@@ -1,33 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.eventValidation = function (savedEvents, newEvent) {
-    var sameDate = savedEvents.filter(function (uncheckedEvent) {
-        return uncheckedEvent.year === newEvent.year &&
-            uncheckedEvent.month === newEvent.month &&
-            uncheckedEvent.date === newEvent.date;
-    });
-    var sameStart = sameDate.find(function (event) { return event.startTime === newEvent.startTime; });
-    var checkStart = sameDate.find(function (event) {
-        return event.startTime >= newEvent.startTime &&
-            event.startTime <= newEvent.endTime;
-    });
-    var checkEnd = sameDate.find(function (event) {
-        return event.endTime > newEvent.startTime && event.endTime <= newEvent.endTime;
-    });
-    if (sameDate.length === 0) {
-        return undefined;
-    }
-    else if (sameStart !== undefined) {
-        return sameStart;
-    }
-    else if (checkStart !== undefined) {
-        return checkStart;
-    }
-    else if (checkEnd !== undefined) {
-        return checkEnd;
-    }
-    else {
-        return undefined;
-    }
+    var sameYear = savedEvents.filter(function (event) { return event.year === newEvent.year; });
+    var sameMonth = sameYear.filter(function (event) { return event.month === newEvent.month; });
+    var sameDates = sameMonth.filter(function (event) { return event.date === newEvent.date; });
+    console.log(sameDates);
+    var timeConflicts = sameDates.length >= 1
+        ? sameDates.find(function (event) {
+            if (event.startTime === newEvent.startTime) {
+                return true;
+            }
+            else if (event.startTime < newEvent.startTime &&
+                event.endTime > newEvent.startTime) {
+                return true;
+            }
+            else if (event.endTime > newEvent.startTime &&
+                event.startTime < newEvent.startTime) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        })
+        : undefined;
+    console.log(timeConflicts);
+    return timeConflicts;
 };
 //# sourceMappingURL=eventValidation.js.map
--- server/src/Utils/eventValidation.ts
+++ server/src/Utils/eventValidation.ts
@@ -4,37 +4,78 @@ export const eventValidation: Function = (
   savedEvents: IEvent[],
   newEvent: IEvent
 ): IEvent => {
-  const sameDate: IEvent[] = savedEvents.filter(
-    (uncheckedEvent: IEvent): boolean =>
-      uncheckedEvent.year === newEvent.year &&
-      uncheckedEvent.month === newEvent.month &&
-      uncheckedEvent.date === newEvent.date
+  const sameYear: IEvent[] = savedEvents.filter(
+    (event: IEvent): boolean => event.year === newEvent.year
   );
-
-  const sameStart: IEvent = sameDate.find(
-    (event: IEvent): boolean => event.startTime === newEvent.startTime
+  const sameMonth: IEvent[] = sameYear.filter(
+    (event: IEvent): boolean => event.month === newEvent.month
   );
-
-  const checkStart: IEvent = sameDate.find(
-    (event: IEvent): boolean =>
-      event.startTime >= newEvent.startTime &&
-      event.startTime <= newEvent.endTime
+  const sameDates: IEvent[] = sameMonth.filter(
+    (event: IEvent): boolean => event.date === newEvent.date
   );
+  console.log(sameDates);
 
-  const checkEnd: IEvent = sameDate.find(
-    (event: IEvent): boolean =>
-      event.endTime > newEvent.startTime && event.endTime <= newEvent.endTime
-  );
-
-  if (sameDate.length === 0) {
-    return undefined;
-  } else if (sameStart !== undefined) {
-    return sameStart;
-  } else if (checkStart !== undefined) {
-    return checkStart;
-  } else if (checkEnd !== undefined) {
-    return checkEnd;
-  } else {
-    return undefined;
-  }
+  const timeConflicts: IEvent =
+    sameDates.length >= 1
+      ? sameDates.find(
+          (event: IEvent): boolean => {
+            if (event.startTime === newEvent.startTime) {
+              return true;
+            } else if (
+              event.startTime < newEvent.startTime &&
+              event.endTime > newEvent.startTime
+            ) {
+              return true;
+            } else if (
+              event.endTime > newEvent.startTime &&
+              event.startTime < newEvent.startTime
+            ) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+        )
+      : undefined;
+  console.log(timeConflicts);
+  return timeConflicts;
 };
+
+// export const eventValidation: Function = (
+//   savedEvents: IEvent[],
+//   newEvent: IEvent
+// ): IEvent => {
+//   const sameDate: IEvent[] = savedEvents.filter(
+//     (uncheckedEvent: IEvent): boolean =>
+//       uncheckedEvent.year === newEvent.year &&
+//       uncheckedEvent.month === newEvent.month &&
+//       uncheckedEvent.date === newEvent.date
+//   );
+
+//   const sameStart: IEvent = sameDate.find(
+//     (event: IEvent): boolean => event.startTime === newEvent.startTime
+//   );
+
+//   const checkStart: IEvent = sameDate.find(
+//     (event: IEvent): boolean =>
+//       event.startTime >= newEvent.startTime &&
+//       event.startTime <= newEvent.endTime
+//   );
+
+//   const checkEnd: IEvent = sameDate.find(
+//     (event: IEvent): boolean =>
+//       event.endTime > newEvent.startTime && event.endTime <= newEvent.endTime
+//   );
+
+//   if (sameDate.length === 0) {
+//     return undefined;
+//   } else if (sameStart !== undefined) {
+//     return sameStart;
+//   } else if (checkStart !== undefined) {
+//     return checkStart;
+//   } else if (checkEnd !== undefined) {
+//     return checkEnd;
+//   } else {
+//     return undefined;
+//   }
+// };
